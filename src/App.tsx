95c47e90026aa859ca46e6b506c8b55cea7dd8e5
Sinan <staylan123@gmail.com>
2025-06-10 13:22:22 -0400

    create temp navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import NavbarComponent from './components/Navbar'
 
 function App() {
 
   return (
     <>
-
+      <NavbarComponent />
     </>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Container, Navbar, Nav } from "react-bootstrap";
+import { IoGameController } from "react-icons/io5";
 
-const Navbar = () => {
+const NavbarComponent = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Navbar expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand
+          href="#home"
+          className="d-flex align-items-center gap-2 brand-color"
+        >
+          <IoGameController size={24} />
+          <span>Crit Deals</span>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default Navbar
+export default NavbarComponent;

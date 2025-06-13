d1065aed76dc088185c48b26cc8397c55cb0dec9
Sinan <staylan123@gmail.com>
2025-06-13 13:42:40 -0400

    make deal responsive

--- src/components/DealInfo.tsx
+++ src/components/DealInfo.tsx
@@ -9,32 +9,44 @@ const DealInfo = ({ deal }: { deal: Deal }) => {
   const discountAsNumber = Number(deal.savings) || 0;
 
   const handleBuy = () => {
-    const url = `https://www.cheapshark.com/redirect?dealID=${(deal.dealID)}`;
-    window.open(url, '_blank', 'noopener,noreferrer');
-  }
+    const url = `https://www.cheapshark.com/redirect?dealID=${deal.dealID}`;
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
 
   return (
-    <Container className="p-3 d-flex gap-4 align-items-center rounded" style={{background: '#454545'}}>
-      <img
-        src={`https://www.cheapshark.com${storeInfo?.images.icon}`}
-        alt="store-icon"
-        style={{ height: "24px", width: "24px" }}
-      />
-      <p className="fw-semibold mb-0">{storeInfo?.storeName}</p>
-      <div className="d-flex align-items-center gap-4 ms-auto">
-        {deal.price < deal.retailPrice && (
-          <p className="fw-bold mb-0 fs5 text-decoration-line-through">
-            ${deal.retailPrice}
-          </p>
-        )}
-        <p className="fw-bold mb-0 fs-3">${deal.price}</p>
+    <Container
+      className="p-3 d-flex flex-column gap-4 rounded flex-md-row align-items-md-center"
+      style={{ background: "#454545" }}
+    >
+      <div className="d-flex align-items-center gap-2 gap-md-4 m-auto m-md-0">
+        <img
+          src={`https://www.cheapshark.com${storeInfo?.images.icon}`}
+          alt="store-icon"
+          style={{ height: "24px", width: "24px" }}
+        />
+        <p className="fw-semibold mb-0">{storeInfo?.storeName}</p>
+      </div>
+      <div className="d-flex align-items-center gap-4 m-auto flex-md-grow-1 justify-content-md-end">
+        <div className="d-md-flex align-items-md-center gap-md-2">
+          {Number(deal.price) < Number(deal.retailPrice) && (
+            <p className="fw-bold mb-0 fs5 text-decoration-line-through">
+              ${deal.retailPrice}
+            </p>
+          )}
+          <p className="fw-bold mb-0 fs-3">${deal.price}</p>
+        </div>
         {discountAsNumber > 0 && (
           <span className="fs-4 fw-semibold px-2 bg-danger rounded text-white">
             {Math.ceil(discountAsNumber)}%
           </span>
         )}
       </div>
-      <button onClick={handleBuy} className="bg-brand text-white rounded py-2 px-4 fw-bold"><BiLinkExternal /> Buy Now</button>
+      <button
+        onClick={handleBuy}
+        className="bg-brand text-white rounded py-2 px-4 fw-bold"
+      >
+        <BiLinkExternal /> Buy Now
+      </button>
     </Container>
   );
 };
--- src/components/GameInfo.tsx
+++ src/components/GameInfo.tsx
@@ -7,8 +7,8 @@ const GameInfoCard = ({ gameInfo }: { gameInfo: GameItemDetailed | null}) => {
 
   return (
     <Container className="text-white p-4 rounded bg-dark d-flex flex-column gap-4 my-5">
-        <h2>{gameInfo.info.title}</h2>
-        <img style={{height: '100px', width: '200px'}} src={gameInfo.info.thumb} alt={gameInfo.info.title} />
+        <h3 className="text-center text-md-start">{gameInfo.info.title}</h3>
+        <img className='m-auto m-md-0' style={{height: '100px', maxWidth: '200px'}} src={gameInfo.info.thumb} alt={gameInfo.info.title} />
         <div className="d-flex flex-column gap-2">
           {
             gameInfo.deals.map(deal => <DealInfo key={deal.dealID} deal={deal}/>)
